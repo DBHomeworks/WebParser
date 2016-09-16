8d6cba4e13f3904ab90a8f2ccf315b543045d06a
TarasLavrenyuk <lavrik1997@gmail.com>
2016-09-16 22:51:59 +0300

    1 task.

--- Program.py
+++ Program.py
@@ -1,22 +1,62 @@
-import lxml.etree as ET
+from lxml import html
+import requests
+from lxml import etree
+
+def empty_string(str):
+    for symbol in str:
+        if symbol is not " ":
+            continue
+        else:
+            return 1
+    return 0
+
+def parse_page():
+    root = etree.Element('data')
+    doc = etree.ElementTree(root)
+
+    page = requests.get('http://tsn.ua')
+    tree = html.fromstring(page.content)
+    link = tree.xpath('//a[not(@class)]/@href')
+
+    print link
+
+    page_number = 0
+    i = 0
+
+    while page_number != 20:
+        if 'javascript' in link[i]:
+            i += 1
+            continue
+
+        if link[i].find('http', 0) == -1:
+            link[i] = 'http://tsn.ua' + link[i]
+
+        page = requests.get(link[i])
+        tree = html.fromstring(page.content)
+        text = tree.xpath('//*[text()]')
+        images = tree.xpath('//img/@src')
 
 
-tree = ET.fromstring('''<div>
-... <div>
-...     <p>
-...     <span class="abc">Monitor</span> <b>$300</b>
-...     </p>
-...     <a href="/add">Add to cart</a>
-... </div>
-... <div>
-...     <p>
-...     <span class="abc">Keyboard</span> $20
-...     </p>
-...     <a href="/add">Add to cart</a>
-... </div>
-... </div>''')
-print tree.xpath('//div[a[contains(., "Add to cart")]]/p//text()')
-# ['\n    ', 'Monitor', ' ', '$300', '\n    ', '\n    ', 'Keyboard', ' $20 \n    ']
-# res = _
-# [txt for txt in (txt.strip() for txt in res) if txt]
-# ['Monitor', '$300', 'Keyboard', '$20']
+        if text and images:
+            page_element = etree.SubElement(root, 'page', url=link[i])
+
+            for t in text:
+                if t.tag != "head" and t.tag != "script" and t.text is not None and "\n" not in t.text and t is not None and "\n" not in t and t.tag != "script" and t.text and not empty_string(t.text):
+                    # print t.text
+                    text_fragment = etree.SubElement(page_element, 'fragment', type='text')
+                    text_fragment.text = t.text
+
+
+            for image in images:
+                # print image
+                image_fragment = etree.SubElement(page_element, 'fragment', type='image')
+                image_fragment.text = image
+
+            page_number += 1
+        i += 1
+
+
+    doc.write('data.xml', xml_declaration=True, encoding='utf-16')
+
+
+parse_page()
